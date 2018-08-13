ed21c5421ae5a41ba19e2dfd7005a0b2f47be52d
Chadwick Wingrave <cwingrav@cw.local>
2018-08-13 15:21:51 -0400

    Caps.

--- src/listener.js
+++ src/listener.js
@@ -1,7 +1,7 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const path       = require('path');
-const Handlebars = require('Handlebars');
+const Handlebars = require('handlebars');
 
 
 /**
